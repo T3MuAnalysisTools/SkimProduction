1761e37cbd762031b8e7a4cb34f233f03011d34d
Vladimir <vladimir.cherepanov@cern.ch>
2019-01-29 14:53:08 +0100

    add crab submitter

---
+++ CRAB/python/NtupleConfig_cff.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+def setupTauNtuple(process):
+    process.load("SkimProduction.CRAB.DsTauNtuple_cfi")
---
+++ CRAB/runNtuple.py
@@ -0,0 +1,105 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+import subprocess
+import sys
+
+options = VarParsing.VarParsing()
+options.register('globalTag',
+                 '92X_dataRun2_Prompt_v5', #default value
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Global Tag")
+
+options.register('nEvents',
+                 500, #default value
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "Maximum number of processed events")
+
+options.register('eosInputFolder',
+                 '/store/relval/CMSSW_8_0_20/RelValZMM_13/GEN-SIM-RECO/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v4_Tr4GT_v4-v1/00000', #default value
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "EOS folder with input files")
+
+options.register('ntupleName',
+                 './DsT3MNtuple.root', #default value
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Folder and name ame for output ntuple")
+
+options.register('runOnMC',
+                 False, #default value
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
+                 "Run on DATA or MC")
+
+options.register('hltPathFilter',
+                 "all", #default value
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Filter on paths (now only accepts all or IsoMu20)")
+
+if options.hltPathFilter == "all" :
+    pathCut   = "all"
+    filterCut = "all"
+elif options.hltPathFilter == "IsoMu20" :
+    pathCut   = "HLT_IsoMu20_v"
+    if options.runOnMC :
+        filterCut = "hltL3crIsoL1sMu16L1f0L2f10QL3f20QL3trkIsoFiltered0p09"
+    else :
+        filterCut = "hltL3crIsoL1sMu18L1f0L2f10QL3f20QL3trkIsoFiltered0p09"
+        
+else :
+    print "[" + sys.argv[0] + "]:", "hltPathFilter=", options.hltPathFilter, "is not a valid parameter!"
+    sys.exit(100)
+
+
+process = cms.Process("DsTauNtuple")
+
+
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10000)
+
+process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
+process.load('Configuration.Geometry.GeometrySimDB_cff')
+
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag.globaltag = cms.string(options.globalTag)
+
+process.load('RecoMET.METFilters.badGlobalMuonTaggersAOD_cff')
+#switch on tagging mode:
+process.badGlobalMuonTagger.taggingMode = cms.bool(True)
+process.cloneGlobalMuonTagger.taggingMode = cms.bool(True)
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.nEvents)  )
+process.source = cms.Source("PoolSource",
+        fileNames = cms.untracked.vstring(),
+        secondaryFileNames = cms.untracked.vstring()
+)
+
+
+process.TFileService = cms.Service('TFileService',
+                                   fileName = cms.string("DsT3MNtuple.root")
+                                   )
+
+process.source.fileNames = ['/store/data/Run2017F/DoubleMuonLowMass/AOD/09May2018-v1/60001/6058D7E9-FEB0-E811-9911-001517642260.root']#/store/data/Run2017F/DoubleMuonLowMass/AOD/17Nov2017-v1/10000/EEC5ACC2-B802-E811-AF14-0CC47A0AD498.root'] # data file for debugs and test runsa
+
+
+####################### TauNtuple ######################
+from SkimProduction.CRAB.NtupleConfig_cff import setupTauNtuple
+setupTauNtuple(process)
+
+
+process.tagger = cms.Path(process.badGlobalMuonTagger)
+process.DsTauNtuple = cms.Sequence(process.T3MTree)
+process.p = cms.Path(process.DsTauNtuple)
+process.schedule = cms.Schedule(process.tagger, process.p)
