a69b2f1fe877ecbe0a6157b3237a79474dee03f7
Vladimir <vladimir.cherepanov@cern.ch>
2020-01-10 14:42:40 +0100

    add SetupBrill

---
+++ CRAB/Lumi/SetupBrill.sh
@@ -0,0 +1,9 @@
+  export PATH=$HOME/.local/bin:/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin:$PATH
+  export PATH=$HOME/.local/bin:/nfshome0/lumipro/brilconda/bin:$PATH
+  pip uninstall brilws
+  pip install --install-option="--prefix=$HOME/.local" brilws
+  pip show brilws
+  brilcalc --version
+  brilcalc lumi -b "STABLE BEAMS" -u /fb -i processedLumis.json --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json
+
+
